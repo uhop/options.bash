bfb582c12f63cd757e006b324c75ee6842bb0ce7
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-04-02 21:27:21 -0500

    Minor refactoring of `args.sh`.

--- args.sh
+++ args.sh
@@ -27,40 +27,40 @@ if [[ $? -ne 4 ]]; then
   exit 1
 fi
 
-__options_bash_skip_help=1
-__options_bash_skip_version=1
+args_skip_help=1
+args_skip_version=1
 
-__options_bash_program_name=""
-__options_bash_program_version=""
-__options_bash_program_description=""
+args_program_name=""
+args_program_version=""
+args_program_description=""
 
-declare -A __options_bash_command_names
-declare -A __options_bash_command_descriptions
-declare -A __options_bash_command_has_arg
-declare -A __options_bash_command_aliases
-__options_bash_check_commands=1
+declare -A args_names
+declare -A args_descriptions
+declare -A args_option_has_arg
+declare -A args_aliases
+args_check_command=1
 
-declare -A __options_bash
-__options_bash_args=""
-__options_bash_command=""
+declare -A args_options
+args_cleaned=""
+args_command=""
 
-define_program() {
+args::program() {
   # name version description
-  __options_bash_program_name="$1"
-  __options_bash_program_version="$2"
-  __options_bash_program_description="$3"
+  args_program_name="$1"
+  args_program_version="$2"
+  args_program_description="$3"
 }
 
-define_command() {
+args::option() {
   # names description has_arg
   # has_arg: "" - no argument
   # has_arg: "arg" - required argument, where "arg" is the name of the argument
 
   local names
   IFS=', ' read -ra names <<< "$1"
-  __options_bash_command_names["${names[0]}"]="$1"
-  __options_bash_command_descriptions["${names[0]}"]="${2:-}"
-  __options_bash_command_has_arg["${names[0]}"]="${3:-}"
+  args_names["${names[0]}"]="$1"
+  args_descriptions["${names[0]}"]="${2:-}"
+  args_option_has_arg["${names[0]}"]="${3:-}"
 
   for name in "${names[@]}"; do
     if [[ "$name" == "--"* ]]; then
@@ -74,34 +74,34 @@ define_command() {
         exit 1
       fi
     else
-      __options_bash_check_commands=0
+      args_check_command=0
     fi
-    __options_bash_command_aliases["$name"]="${names[0]}"
+    args_aliases["$name"]="${names[0]}"
   done
 }
 
-parse_options() {
+args::parse() {
   local long_options=""
   local short_options=""
 
-  for option in "${!__options_bash_command_aliases[@]}"; do
-    local command="${__options_bash_command_aliases[$option]}"
+  for option in "${!args_aliases[@]}"; do
+    local command="${args_aliases[$option]}"
     if [[ "$option" == "--"* ]]; then
       if [[ -n "$long_options" ]]; then long_options+=","; fi
       long_options+="${option#--}"
-      if [[ -n "${__options_bash_command_has_arg[$command]}" ]]; then long_options+=":"; fi
+      if [[ -n "${args_option_has_arg[$command]}" ]]; then long_options+=":"; fi
     elif [[ "$option" == "-"* ]]; then
       short_options+="${option#-}"
-      if [[ -n "${__options_bash_command_has_arg[$command]}" ]]; then short_options+=":"; fi
+      if [[ -n "${args_option_has_arg[$command]}" ]]; then short_options+=":"; fi
     fi
   done
 
   local parsed
   local status=0
-  parsed=$(getopt -o "$short_options" -l "$long_options" -n "${__options_bash_program_name}" -- "$@") || status=$?
+  parsed=$(getopt -o "$short_options" -l "$long_options" -n "${args_program_name}" -- "$@") || status=$?
 
   if [[ $status -ne 0 ]]; then
-    echo "Try '${__options_bash_program_name} --help' for more information."
+    echo "Try '${args_program_name} --help' for more information."
     exit 1
   fi
 
@@ -111,29 +111,29 @@ parse_options() {
       shift
       break
     fi
-    local option="${__options_bash_command_aliases["$1"]}"
+    local option="${args_aliases["$1"]}"
     if [[ -z "$option" ]]; then
       echo "Error: Unknown option '$1'"
-      echo "Try '${__options_bash_program_name} --help' for more information."
+      echo "Try '${args_program_name} --help' for more information."
       exit 1
     fi
     shift
-    if [[ -n "${__options_bash_command_has_arg[$option]}" ]]; then
-      __options_bash["$option"]="$1"
+    if [[ -n "${args_option_has_arg[$option]}" ]]; then
+      args_options["$option"]="$1"
       shift
     else
-      __options_bash["$option"]=""
+      args_options["$option"]=""
     fi
   done
 
-  if [[ ${__options_bash_check_commands} -eq 0 ]]; then
+  if [[ ${args_check_command} -eq 0 ]]; then
     if [ "$#" -eq 0 ]; then
       echo "Error: No command specified"
-      echo "Try '${__options_bash_program_name} --help' for more information."
+      echo "Try '${args_program_name} --help' for more information."
       exit 1
     fi
     local unknown_command=0
-    for command in "${!__options_bash_command_aliases[@]}"; do
+    for command in "${!args_aliases[@]}"; do
       if [[ "$command" == "-"* ]]; then continue; fi
       if [[ "$command" == "$1" ]]; then
         unknown_command=1
@@ -142,40 +142,40 @@ parse_options() {
     done
     if [[ $unknown_command -eq 0 ]]; then
       echo "Error: Unknown command '$1'"
-      echo "Try '${__options_bash_program_name} --help' for more information."
+      echo "Try '${args_program_name} --help' for more information."
       exit 1
     else
-      __options_bash_command="${__options_bash_command_aliases[$1]}"
+      args_command="${args_aliases[$1]}"
     fi
   fi
 
-  __options_bash_args="$@"
+  args_cleaned="$@"
 }
 
 show_help() {
-  echo "${__options_bash_program_name} ${__options_bash_program_version}"
-  echo "${__options_bash_program_description}"
+  echo "${args_program_name} ${args_program_version}"
+  echo "${args_program_description}"
   echo ""
-  echo "Usage: ${__options_bash_program_name} [options] [command] [command options]"
+  echo "Usage: ${args_program_name} [options] [command] [command options]"
   echo ""
   echo "Options:"
-  for option in "${!__options_bash_command_names[@]}"; do
+  for option in "${!args_names[@]}"; do
     if [[ "$option" == "--"* ]]; then continue; fi
     if [[ "$option" != "-"* ]]; then continue; fi
-    echo "  ${__options_bash_command_names[$option]}"
+    echo "  ${args_names[$option]}"
   done
-  for option in "${!__options_bash_command_names[@]}"; do
+  for option in "${!args_names[@]}"; do
     if [[ "$option" != "--"* ]]; then continue; fi
-    echo "  ${__options_bash_command_names[$option]}"
+    echo "  ${args_names[$option]}"
   done
   echo ""
   echo "Commands:"
-  for command in "${!__options_bash_command_names[@]}"; do
+  for command in "${!args_names[@]}"; do
     if [[ "$command" == "-"* ]]; then continue; fi
-    echo "  ${__options_bash_command_names[$command]}"
+    echo "  ${args_names[$command]}"
   done
 }
 
 show_version() {
-  echo "${__options_bash_program_name} ${__options_bash_program_version}"
+  echo "${args_program_name} ${args_program_version}"
 }
---
+++ tests/manual/test-args.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -o errexit -o pipefail -o nounset -o noclobber
+
+source "$(dirname "$(realpath "$0")")/../../args.sh"
+
+args::program "test" "1.0" "Test program"
+
+args::option "test, t" "Test command"
+args::option "cmd" "Sample command"
+
+args::option "-v, --version" "Show version"
+args::option "-h, --help" "Show help"
+args::option "--option" "Sample long option" arg
+args::option "-o" "Sample short option" arg
+
+# args::option "--args, -, -args" "Bad short option"
+# args::option "--, -z" "Bad long option"
+
+# show_help
+
+# echo ""
+# show_version
+
+args::parse "$@"
+eval set -- "${args_cleaned}"
+
+echo "Current arguments: $@"
+echo "Current command: ${args_command}"
+
+echo "Current options:"
+for option in "${!args_options[@]}"; do
+  echo "  $option: ${args_options[$option]}"
+done
+
+declare -n opt=args_options
+if [[ -v opt["--option"] ]]; then
+  echo "--option: ${opt["--option"]}"
+fi
