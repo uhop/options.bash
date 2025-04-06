13a141f38d58a7d33554134902fc06127f614d6f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-04-06 01:05:10 -0500

    Minor rearrangement + delegating some functions to ANSI functions.

--- string.sh
+++ string.sh
@@ -4,45 +4,20 @@ set -o errexit -o pipefail -o nounset -o noclobber
 
 string::clean() {
   local string="$1"
-  if [ -v ansi::strip ]; then
+  if type ansi::strip >/dev/null 2>&1; then
     ansi::strip "$string"
   else
     echo "$string"
   fi
 }
 
-string::err() {
-  if [[ -t 2 || -z "$TERM" ]]; then
-    echo -e "$@" >&2
-    return 1
-  fi
-
-  local args=()
-  for arg in "$@"; do
-    args+=("$(string::clean "$arg")")
-  done
-  echo -e "${args[@]}" >&2
-  return 1
-}
-
-string::out() {
-  if [[ -t 1 || -z "$TERM" ]]; then
-    echo -e "$@"
-    return 0
-  fi
-
-  local args=()
-  for arg in "$@"; do
-    args+=("$(string::clean "$arg")")
-  done
-  echo -e "${args[@]}"
-  return 0
-}
-
 string::length() {
   local string="$1"
-  local string_clean=$(string::clean "$string")
-  echo "${#string_clean}"
+  if type ansi::length >/dev/null 2>&1; then
+    ansi::length "$string"
+  else
+    echo "${#string}"
+  fi
 }
 
 string::make_pad() {
@@ -90,5 +65,33 @@ string::pad() {
   local pad_left_str=$(string::make_pad "$pad_left" "$pad")
   local pad_right_str=$(string::make_pad "$pad_right" "$pad")
 
-  printf "%s%s%s" "$pad_left_str" "$string_clean" "$pad_right_str"
+  printf "%s%s%s" "$pad_left_str" "$string" "$pad_right_str"
+}
+
+string::err() {
+  if [[ -t 2 || -z "$TERM" ]]; then
+    echo -e "$@" >&2
+    return 1
+  fi
+
+  local args=()
+  for arg in "$@"; do
+    args+=("$(string::clean "$arg")")
+  done
+  echo -e "${args[@]}" >&2
+  return 1
+}
+
+string::out() {
+  if [[ -t 1 || -z "$TERM" ]]; then
+    echo -e "$@"
+    return 0
+  fi
+
+  local args=()
+  for arg in "$@"; do
+    args+=("$(string::clean "$arg")")
+  done
+  echo -e "${args[@]}"
+  return 0
 }
