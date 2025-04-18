465b97c5aa5dd4f743d4229c5cad37026e1e42dc
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-04-18 16:52:07 -0500

    Added support for multiple cap names.

--- tests/manual/tput.sh
+++ tests/manual/tput.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
-cap_name="$1"
-string="$(tput "$@")"
+set -o errexit -o pipefail -o nounset -o noclobber
 
-# lines="$(printf "op\nsetab 1\nsetaf 2\nsgr0\n")"
-# string="$(tput -S <<< "$lines")"
+names=()
+input=''
+while true; do
+  if [[ $# -eq 0 ]]; then break; fi
+  arg="$1"
+  shift
+  echo "Arg: $arg"
+  names+=("$arg")
+  input+="${arg}\nlongname\n"
+done
 
-encode() {
-  local escaped="${1@Q}"
-  echo "${escaped:2:-1}"
-}
+delimiter="$(tput longname)"
+replacement=$'\x01'  # A non-printing character (you can use others)
 
-echo "$@: $(encode "$string")"
+output="$(tput -S <<< $(echo -e "$input"))"
+
+# Escape the backslashes in the string for proper replacement
+escaped_string="${output//'\\'/'\\\\'}"
+
+# Replace delimiter with a single character that's not in the string
+modified_string="${escaped_string//$delimiter/$replacement}"
+
+set +e
+IFS="$replacement" read -rd '' -a array <<< "$modified_string"
+set -e
+
+# Output the resulting array
+for i in "${!names[@]}"; do
+  echo "${names[$i]}: ${array[$i]@Q}"
+done
