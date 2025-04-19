08cdbd0d26bc5e02f392e77cd1c4726b48cb61fd
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2025-04-19 00:49:26 -0500

    Added spaces.

--- tests/manual/test-ansi.sh
+++ tests/manual/test-ansi.sh
@@ -11,6 +11,6 @@ ansi::out "${BLUE}Blue${RESET_ALL}"
 ansi::out "$(ansi::style::make \
   bold italic \
   "$(ansi::c256::fg_true 0 128 64)" \
-  "$(ansi::c256::bg_true 255 255 128)")Bold$(ansi::style::make reset_all)"
+  "$(ansi::c256::bg_true 255 255 128)") Bold $(ansi::style::make reset_all)"
 
 # echo -en "$(ansi::screen::scroll_up 3)$(ansi::screen::scroll_down 2)"
